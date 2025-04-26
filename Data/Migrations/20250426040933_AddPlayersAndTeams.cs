df79b2fcabc09779f3e2f7dd07726f1c1040bfe4
jos <josmerjauriacaceria@gmail.com>
2025-04-25 23:30:53 -0500

    funcionalidad asignar jugadores a equipos, listar

---
+++ Data/Migrations/20250426040933_AddPlayersAndTeams.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace examenparcialjj.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPlayersAndTeams : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
